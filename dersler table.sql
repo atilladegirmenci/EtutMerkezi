a1f1ca26e7dc4df111dbf248d8095edea2f76b43
atilladegirmenci <141858642+atilladegirmenci@users.noreply.github.com>
2024-12-19 22:12:38 +0300

    Delete dersler table.sql

--- dersler table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Dersler(
-dersID INT  IDENTITY(1,1) primary key,
-ders_ad varchar(20) ,
-ogretmenID INT   constraint fk_Ogretmenler foreign key references Ogretmenler(ogretmenID),
-kontenjan INT
-);
