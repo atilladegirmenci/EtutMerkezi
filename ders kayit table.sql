e0c54b7a308fb081f047d2d9b01392bea273e9cf
atilladegirmenci <141858642+atilladegirmenci@users.noreply.github.com>
2024-12-19 22:12:24 +0300

    Delete ders kayit table.sql

--- ders kayit table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Ders_Kayit(
-kayitID INT IDENTITY(1,1) primary key,
-ogrenciID INT constraint fk_Ders_kayit_OgrenciID foreign key references Ogrenci(Ders_kayit_OgrenciID),
-dersID INT constraint fk_Dersler foreign key references Dersler(dersID)
-);
